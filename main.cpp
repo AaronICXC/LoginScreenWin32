af7d7a4f26339373350f81a54d301c3698571366
Aaron R <aaron.akr@yahoo.com>
2024-03-20 00:45:17 -0500

    WIndow

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <windows.h>
+
+// procedure
+LRESULT CALLBACK wmProcedure(HWND, UINT, WPARAM, LPARAM);
+
+HWND hParent;
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE prevInst, LPSTR args, int ncmdshow) {
+    ncmdshow = 0;
+
+    WNDCLASSW wm = {0};
+
+    {
+        wm.hbrBackground = (HBRUSH)COLOR_WINDOW;
+        wm.hCursor = LoadCursor(NULL, IDC_ARROW);
+        wm.hInstance = hInst;
+        wm.lpszClassName = L"loginProject";
+        wm.lpfnWndProc = wmProcedure;
+    }
+
+    if (!(RegisterClassW(&wm)))
+        return -1;
+
+    hParent = CreateWindowW(L"loginProject", L"Login", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 500, 500, NULL, NULL, NULL, NULL);
+
+    MSG msg = {0};
+
+    while (GetMessage(&msg, NULL, NULL, NULL)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+}
+
+LRESULT CALLBACK wmProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
+    switch (msg) {
+        case WM_CREATE:
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            return DefWindowProcW(hWnd, msg, wp, lp);
+            break;
+    }
+}
